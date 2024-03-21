eb3919c19d6934bc03e0b617bbac9913898cb196
asmita <asmitathapa429@gmail.com>
2024-03-21 17:28:21 +0545

    this is simple linking of .js file with .html file.

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+console.log("Hello Web Developer!");
+let a = 10;
+let b = 5;
+console.log("sum is :", a+b);
+
+//this is a comment.
+
+//Template Literals: They are used to add embedded expressions in a string.
